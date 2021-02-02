eaf5a8601bea0608c5f02955dd69884cd2d46ade
nagytommy76 <nagytommy76@gmail.com>
2021-02-02 20:47:13 +0100

    upload files

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from 'axios'
 
-createApp(App)
+const app = createApp(App)
   .use(store)
   .use(router)
   .mount("#app");
+app.config.globalProperties.axios=axios
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
   </div>
 </template>
+<script>
+export default {
+  mounted(){
+    this.axios.get('https://api.spoonacular.com/recipes/findByIngredients?apiKey=820c397d13094ee6a0e1780f715b0558&ingredients=apples,+flour,+sugar&number=2')
+    .then(test =>console.log(test))
+  },
+}
+</script>
